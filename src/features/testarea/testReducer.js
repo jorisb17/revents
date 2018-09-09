be2a80383c873b6252f299f460ddd5f694613078
Joris Bruinsma <jorisb17@gmail.com>
2018-09-09 17:17:17 +0200

    added loading screen component

--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,4 +1,4 @@
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './testConstants';
+import { INCREMENT_COUNTER, DECREMENT_COUNTER, COUNTER_ACTION_STARTED, COUNTER_ACTION_FINISHED } from './testConstants';
 import { createReducer } from '../../app/common/util/reducerUtil'
 
 const initialState = {
@@ -11,6 +11,14 @@ export const incrementCounter = (state, payload) => {
 
 export const decrementCounter = (state, payload) => {
   return { ...state, data: state.data - 1 };
+}
+
+export const counterActionStarted = (state, payload) =>{
+  return {...state, loading: true}
+}
+
+export const counterActionFinished = (state, payload) =>{
+  return {...state, loading: false}
 }
 
 // const testReducer = (state = initialState, action) => {
@@ -26,5 +34,7 @@ export const decrementCounter = (state, payload) => {
 
 export default createReducer(initialState, {
   [INCREMENT_COUNTER]: incrementCounter,
-  [DECREMENT_COUNTER]: decrementCounter
+  [DECREMENT_COUNTER]: decrementCounter,
+  [COUNTER_ACTION_STARTED]: counterActionStarted,
+  [COUNTER_ACTION_FINISHED]: counterActionFinished
 });
